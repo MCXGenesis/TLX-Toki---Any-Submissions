08489e35e5753de5513d41c0d68d9b033a46ec27
MCXGenesis <naufal.maula@gmail.com>
2026-02-16 12:33:32 +0700

    add troc#42 a

---
+++ TROC #42/A. Satu Dua Tiga Empat/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    if (x == 1){
+        cout << "1";
+    }
+    else if ( x == 2){
+        cout << "3";
+    }
+    else if (x == 3){
+        cout << "6";
+    }
+    else {
+        cout << "0";
+    }
+}
